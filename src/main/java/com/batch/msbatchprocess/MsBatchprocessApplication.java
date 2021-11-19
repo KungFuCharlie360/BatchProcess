6ed3d79b21da9f43a07f551fea736145f131d7a8
bradcortez23 <brad.cortez23@gmail.com>
2021-11-19 14:55:10 +0800

    Initial Commit

---
+++ src/main/java/com/batch/msbatchprocess/MsBatchprocessApplication.java
@@ -0,0 +1,13 @@
+package com.batch.msbatchprocess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBatchprocessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBatchprocessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/batch/msbatchprocess/MsBatchprocessApplicationTests.java
@@ -0,0 +1,12 @@
+package com.batch.msbatchprocess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class MsBatchprocessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
